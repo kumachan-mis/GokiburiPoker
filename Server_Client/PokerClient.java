d2a7c562973bcde009e4b12dad4abfbf335bdb8b
kumachan-mis <yuyachorus-97.11nov@ezweb.ne.jp>
2018-04-08 11:52:31 +0900

    change a little

--- Server_Client/PokerClient.java
+++ Server_Client/PokerClient.java
@@ -20,48 +20,60 @@ public class PokerClient{
         PORT = Integer.parseInt(args[0]);
         addr = InetAddress.getByName(args[1]);
         System.out.println("アドレス : " + addr);
-        socket = new Socket(addr, PORT);
 
-        sc = new Scanner(System.in);
-
-        reader = 
-        new BufferedReader(
-            new InputStreamReader(
-                socket.getInputStream()
-                )
-        );
-        writer = 
-        new PrintWriter(
-            new BufferedWriter(
-                new OutputStreamWriter(
-                    socket.getOutputStream()
-                    )
-            ), true
-        );
-
-        initialize();
+        makeInstance();
+        sendNickname();
 
         sc.close();
         socket.close();
     }
 
-    private static void initialize(){
+    private static void makeInstance(){
+        sc = new Scanner(System.in);
+
+        try{
+            socket = new Socket(addr, PORT);
+
+            reader = 
+            new BufferedReader(
+                new InputStreamReader(
+                    socket.getInputStream()
+                )
+            );
+            
+            writer = 
+            new PrintWriter(
+                new BufferedWriter(
+                    new OutputStreamWriter(
+                        socket.getOutputStream()
+                    )
+                ), true
+            );
+        }catch(IOException e){
+            System.err.println(e);
+        }
+    }
+
+    private static void sendNickname(){
         String nickName = null;
         boolean correctName = false;
 
         while(correctName == false){
             System.out.print("ニックネームを入力してください: ");
             nickName = sc.next();
-            System.out.print("ニックネームが " + nickName + " でよければ 0 を入力してください: ");
-            correctName = (sc.next().equals("0"));
+            System.out.print("ニックネームが " + nickName + " でよければ y を入力してください: ");
+            String reply = sc.next();
+            correctName = (reply.equals("y") || reply.equals("Y"));
         }
 
         writer.println(nickName);
+
         try{
             System.out.println(reader.readLine());
         }catch(IOException e){
             System.err.println(e);
             System.err.println("サーバ " + addr + " との接続が切れました");
+            return;
         }
     }
 }
